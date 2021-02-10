f6a4756e6ec00fc1428e47adaa0ed817c6f61f5e
devappliberated <dev@appliberated.com>
2021-02-10 11:56:16 +0200

    * Improve sidebar menu layout and style * Improve inner app page layout with left and right sidebars

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
       colors: {
         'dark-footer': '#151515',
       },
+      maxWidth: {
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+       },
     },
   },
   variants: {
